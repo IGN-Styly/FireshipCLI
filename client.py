1a7bbcc9b68ca54970f9f069d5cc19596c5c5228
IGN-Styly <claudioresendeptbr@gmail.com>
2022-01-17 23:44:44 -0300

    Started work on ecrypted json data.

--- client.py
+++ client.py
@@ -2,6 +2,6 @@ import requests
 import json
 import crypt
 import creds
-
+data=crypt.encrypt(creds.json_data.encode('utf-8'))
 a=requests.post(url="http://127.0.0.1:5000/send", data=creds.json_data)
 print(a.content)
